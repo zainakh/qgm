9075c3db9cf2f05cf8041b7aa595fb9a023c6700
Zain Khan <zain.kha.al@gmail.com>
2023-04-04 18:45:31 -0400

    First commit

---
+++ R/plots.R
@@ -0,0 +1,75 @@
+#' Converts the pcalgo graph object into an igraph object and plots it.
+#'
+#' Constructs igraph object from graphnel type and adds weights (if passed in).
+#' Default layout is in a circle so graphs are easily comparable.
+#'
+#' @param g A graph object of class graph
+#' @param weights A list of weights for edges in the graph
+#' @param layout A layout to organize the graph with
+#' @param vertex.size A node size for the graph
+#' @param vertex.color Specify the color of nodes in the graph
+#' @return
+igplot <- function(g, weights=TRUE, layout=igraph::layout_in_circle,
+                   vertex.size=50, vertex.color="lightblue",...){
+
+  g <- igraph::graph_from_graphnel(as(g, "graphNEL"))
+  op <- par(mar=c(1,1,1,1))
+
+  if (weights == TRUE){
+    ew <- round(igraph::get.edge.attribute(g,"weight"),2)
+    igraph::plot.igraph(g,layout=layout,edge.label=ew,edge.label.cex=0.9, vertex.size=vertex.size,vertex.color=vertex.color,...)
+  }
+  else {
+    igraph::plot.igraph(g,layout=layout,vertex.size=vertex.size,vertex.color=vertex.color,...)
+  }
+
+  par(op) # Reset to previous settings
+}
+
+#' Calculate the graph of variable relationships at a particular quantile level.
+#'
+#' It assumes the input dataframe has at least two columns (columns are variables) and that
+#' the quantile level is between 0 and 1 (non-inclusive). Additionally it assumes all
+#' column names are unique.
+#'
+#' This function calls the pc algorithm with the quantile association hypothesis test.
+#' Afterwards, it constructs the marginal associations of all variables and uses their
+#' marginal associations as edge weights in the returned graph.
+#'
+#' An igraph plot is plotted with the marginal associations at the quantile level
+#' prescribed.
+#'
+#' @param data A data frame of data with unique column/rownames
+#' @param tau A particular quantile level (0 to 1, not inclusive)
+#' @return
+#' @examples
+#' plot.quantile.dag(df, tau=0.5)
+plot.quantile.dag <- function(data, tau, verbose=FALSE) {
+  if(tau <= 0 | tau >= 1) {
+    return("Tau must be within 0 and 1 (non-inclusive)")
+  }
+
+  saveRDS(tau, "tau.rds")
+  data <- jitter.columns(data, factor=0.1)
+  pc_graph <- pcalg::pc(data, indepTest = quantile.ztest, labels = colnames(data), alpha = 0.05, verbose = verbose)
+
+  res <- marginal.test(data, tau=tau)
+
+  colnames(lookup_table) <- colnames(data)
+  rownames(lookup_table) <- colnames(data)
+
+  data_var = pc_graph@graph@edgeData@data
+  edgenames = names(data_var)
+
+  for(i in 1:length(edgenames)) {
+    curredge = edgenames[i]
+    varnames = strsplit(curredge, split="|", fixed=TRUE)
+    weightval = lookup_table[ varnames[[1]][1], varnames[[1]][2] ]
+    data_var[[ curredge ]]$weight = weightval
+  }
+
+  pc_graph@graph@edgeData@data = data_var
+  unlink("tau.rds")
+
+  igplot(pc_graph@graph, weights=TRUE)
+}
---
+++ R/quantile_association.R
@@ -0,0 +1,124 @@
+#' Adds jitter to the input dataframe to break ties.
+#'
+#' Goes column by column in the dataframe and adds jitter from the distribution
+#'
+#' Unif(-a, a)
+#'
+#' where a = factor * d /5 (and d is the minimum distance between any two points in a vector).
+#'
+#'
+#' @param data Input dataframe of data
+#' @param factor A float to scale the jittering by (if greater than 2.5, there is a risk of quantile crossings)
+#' @return
+jitter.columns <- function(data, factor=0.1) {
+  num_cols <- length((colnames(data)))
+
+  for(x in 1:num_cols) {
+    data[,x] <- jitter(data[,x], factor=factor)
+  }
+  return(data)
+}
+
+
+#' Performs a hypothesis test of two variables given a conditioning set and returns
+#' a p-value on if they are independent (p > 0.05) or not (p <= 0.05).
+#'
+#' Uses a hypothesis test that considers the proportion of times two variables
+#' have concordant (conditional) quantile regression residuals. Counts the
+#' amount of times the residual pairs are concordant (jointly above or below their
+#' respective conditional quantile regression lines) and normalizes that concordant
+#' proportion. Normalization is based on what the proportions would be
+#' under the null hypothesis. From the normalized variable, p values of the
+#' relationship between columns x and y can be returned.
+#'
+#'
+#' @param x Index of a column
+#' @param y Index of a column (not equal to x)
+#' @param S Conditioning set to be used to determine if conditional independence exists (can be empty set)
+#' @param suffStat The dataframe of data (there is no sufficient statistic for this calculation)
+#' @return pvalue corresponding to if the quantile level
+quantile.ztest <- function (x, y, S, suffStat) {
+  tau <- readRDS("tau.rds")
+  n <- length(suffStat[,x])
+
+  var1 <- suffStat[,x]
+  var2 <- suffStat[,y]
+
+  if(length(S) == 0){
+    q1 <- quantreg::rq(var1 ~ 1, tau=tau)
+    q2 <- quantreg::rq(var2 ~ 1, tau=tau)
+
+    fit.q1 <- fitted(q1)
+    fit.q2 <- fitted(q2)
+  }
+  else {
+    Z <- sapply(suffStat[,S], as.numeric)
+
+    q1 <- quantreg::rq(var1 ~ Z, tau=tau)
+    q2 <- quantreg::rq(var2 ~ Z, tau=tau)
+
+    fit.q1 <- fitted(q1)
+    fit.q2 <- fitted(q2)
+  }
+
+  ptilde <- sum((var1 < fit.q1) & (var2 < fit.q2)) / n
+  phat <- sum((var1 > fit.q1) & (var2 > fit.q2)) / n
+
+  zstat_below <- (ptilde - tau^2) / sqrt( tau^2 * (1-tau)^2 / n)
+  zstat_above <- (phat - (1 - tau)^2) / sqrt( tau^2 * (1-tau)^2 / n)
+
+  p_val_below <- 2 * pnorm(abs(zstat_below), lower.tail = FALSE)
+  p_val_above <- 2 * pnorm(abs(zstat_above), lower.tail = FALSE)
+
+  if (tau < 0.5) {
+    return(p_val_below)
+  }
+  else {
+    return(p_val_above)
+  }
+}
+
+#' Returns the marginal quantile association for all pairs of variables in a dataframe.
+#'
+#' Runs a double for loop across all columns and calculates the quantile association
+#' statistic for all possible pairs. Depending on if the quantile level is above or
+#' below the median, the statistic will consider if the residuals are jointly above or
+#' jointly below their respective marginal regression lines.
+#'
+#' @param data Input dataframe of data
+#' @param tau A particular quantile level (0 to 1, not inclusive)
+#' @return An n by n matrix (where n is the number of columns in data) that contains the marginal relationships of each pair of columns
+marginal.test <- function(data, tau) {
+  num_cols <- length((colnames(data)))
+
+  zstat_above <- matrix(0, nrow=num_cols, ncol=num_cols)
+  zstat_below <- matrix(0, nrow=num_cols, ncol=num_cols)
+
+  for(x in 1:num_cols) {
+    for(y in 1:num_cols) {
+
+      n <- length(data[,x])
+
+      var1 <- data[,x]
+      var2 <- data[,y]
+
+      q1 <- quantreg::rq(var1 ~ 1, tau=tau)
+      q2 <- quantreg::rq(var2 ~ 1, tau=tau)
+
+      fit.q1 <- fitted(q1)
+      fit.q2 <- fitted(q2)
+
+      ptilde <- sum((var1 < fit.q1) & (var2 < fit.q2)) / n
+      phat <- sum((var1 > fit.q1) & (var2 > fit.q2)) / n
+
+      zstat_below[x, y] <- (ptilde - tau^2) / sqrt( tau^2 * (1-tau)^2 / n)
+      zstat_above[x, y] <- (phat - (1 - tau)^2) / sqrt( tau^2 * (1-tau)^2 / n)
+
+    }
+  }
+
+  if(tau >= 0.5){
+    return( zstat_above )
+  }
+  return( zstat_below )
+}
